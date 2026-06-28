0941738dde8ff84b45a5b46695487fabf4c1a053
Flávio R. Troszczanczuk <flavioczuk@gmail.com>
2026-06-28 18:28:15 -0300

    adding lemon trail

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,141 +1,23 @@
 'use client'
 
 // libraries
-import { useEffect, useRef } from 'react'
 import { Link } from 'next-transition-router'
-import Image from 'next/image'
 import { usePathname } from 'next/navigation'
 import { gsap } from 'gsap'
 
 // components
 import MagneticButton from '@/components/Utils/Animations/MagneticButton'
+import LemonTrail from '@/components/Utils/Animations/LemonTrail'
 import Logo from '@/components/Svg/Logo'
 
 // utils
-import { contact, pages, social } from '@/utils/routes'
+import { pages, social } from '@/utils/routes'
 import { getYear } from '@/utils/functions'
-
-// lemon images
-import lemon1 from '@/assets/img/lemon-1.png'
-import lemon2 from '@/assets/img/lemon-2.png'
-import lemon3 from '@/assets/img/lemon-3.png'
-import lemon4 from '@/assets/img/lemon-4.png'
-import lemon5 from '@/assets/img/lemon-5.png'
-
-const LEMONS = [lemon1.src, lemon2.src, lemon3.src, lemon4.src, lemon5.src]
 
 export default function Footer() {
 
-	const footerRef = useRef<HTMLElement>(null)
 	const pathname = usePathname()
 	const year = getYear(new Date().getFullYear().toString())
-
-	useEffect(() => {
-		const root = footerRef.current
-		if (!root) return
-
-		let incr = 0, oldIncrX = 0, oldIncrY = 0, firstMove = true, indexImg = 0
-
-		const isCoarsePointer = window.matchMedia('(hover: none)').matches
-		const resetDist = window.innerWidth / (isCoarsePointer ? 6 : 8)
-
-		function applyMove(clientX: number, clientY: number) {
-			const rect = root!.getBoundingClientRect()
-
-			if (firstMove) {
-				firstMove = false
-				oldIncrX = clientX
-				oldIncrY = clientY
-				return
-			}
-
-			incr += Math.abs(clientX - oldIncrX) + Math.abs(clientY - oldIncrY)
-
-			if (incr > resetDist) {
-				incr = 0
-				createLemon(
-					clientX - rect.left,
-					clientY - rect.top,
-					clientX - oldIncrX,
-					clientY - oldIncrY
-				)
-			}
-
-			oldIncrX = clientX
-			oldIncrY = clientY
-		}
-
-		function createLemon(x: number, y: number, deltaX: number, deltaY: number) {
-			const H = root!.offsetHeight
-			if (y > H - 100) return
-
-			const img = document.createElement('img')
-			img.src = LEMONS[indexImg]
-			img.style.cssText = 'width:12vw;height:12vw;min-width:70px;min-height:70px;position:absolute;object-fit:contain;z-index:20;pointer-events:none;'
-			root!.appendChild(img)
-
-			const tl = gsap.timeline({
-				onComplete: () => { root!.removeChild(img); tl.kill() }
-			})
-
-			tl.fromTo(img, {
-				xPercent: -50 + (Math.random() - 0.5) * 80,
-				yPercent: -50 + (Math.random() - 0.5) * 10,
-				scaleX: 1.3,
-				scaleY: 1.3,
-				rotation: (Math.random() - 0.5) * 20
-			}, {
-				scaleX: 1,
-				scaleY: 1,
-				ease: 'elastic.out(2, 0.6)',
-				duration: 0.4
-			})
-
-			tl.fromTo(img, { x }, {
-				x: '+=' + deltaX * 2,
-				rotation: 0,
-				ease: 'power1.in',
-				duration: 0.4
-			}, '<')
-
-			tl.fromTo(img, { y }, {
-				y: '+=' + (H - y),
-				scale: 0.9,
-				yPercent: -95,
-				ease: 'back.in(1.1)',
-				duration: 0.4
-			}, '<')
-
-			tl.to(img, {
-				x: '+=' + deltaX * 1.6,
-				rotation: (Math.random() - 0.5) * 40,
-				ease: 'power1.in',
-				duration: 0.3
-			})
-			tl.to(img, {
-				yPercent: 150,
-				ease: 'back.in(' + (1.5 + (1 - y / H)) + ')',
-				duration: 0.3
-			}, '<')
-
-			indexImg = (indexImg + 1) % LEMONS.length
-		}
-
-		const onMouseMove = (e: MouseEvent) => applyMove(e.clientX, e.clientY)
-		const onTouchMove = (e: TouchEvent) => {
-			if (e.touches?.[0]) applyMove(e.touches[0].clientX, e.touches[0].clientY)
-		}
-
-		root.addEventListener('mousemove', onMouseMove)
-		root.addEventListener('touchstart', onTouchMove, { passive: true })
-		root.addEventListener('touchmove', onTouchMove, { passive: true })
-
-		return () => {
-			root.removeEventListener('mousemove', onMouseMove)
-			root.removeEventListener('touchstart', onTouchMove)
-			root.removeEventListener('touchmove', onTouchMove)
-		}
-	}, [])
 
 	const scrollToTop = (e: React.MouseEvent<HTMLAnchorElement>) => {
 		e.preventDefault()
@@ -166,8 +48,8 @@ export default function Footer() {
 	}
 
 	return (
-		<footer
-			ref={footerRef}
+		<LemonTrail
+			as='footer'
 			className='bg-green-dark rounded-tl-2xl rounded-tr-2xl sm:rounded-tl-4xl sm:rounded-tr-4xl md:rounded-tl-[3rem] md:rounded-tr-[3rem] -mt-4 sm:-mt-8 md:-mt-12 relative z-10 text-green-light overflow-hidden'
 			data-main-footer
 		>
@@ -297,6 +179,6 @@ export default function Footer() {
 
 				</div>
 			</div>
-		</footer>
+		</LemonTrail>
 	)
 }
---
+++ src/components/Utils/Animations/LemonTrail.tsx
@@ -0,0 +1,156 @@
+'use client'
+
+// libraries
+import { useEffect, useRef } from 'react'
+import { gsap } from 'gsap'
+
+// lemon images
+import lemon1 from '@/assets/img/lemon-1.png'
+import lemon2 from '@/assets/img/lemon-2.png'
+import lemon3 from '@/assets/img/lemon-3.png'
+import lemon4 from '@/assets/img/lemon-4.png'
+import lemon5 from '@/assets/img/lemon-5.png'
+
+const LEMONS = [lemon1.src, lemon2.src, lemon3.src, lemon4.src, lemon5.src]
+
+interface LemonTrailProps extends React.HTMLAttributes<HTMLElement> {
+	as?: React.ElementType
+}
+
+function useLemonTrail(containerRef: React.RefObject<HTMLElement | null>) {
+	useEffect(() => {
+		const container = containerRef.current
+		if (!container) return
+
+		const root: HTMLElement = container
+
+		let incr = 0
+		let oldIncrX = 0
+		let oldIncrY = 0
+		let firstMove = true
+		let indexImg = 0
+
+		const isCoarsePointer = window.matchMedia('(hover: none)').matches
+		const resetDist = window.innerWidth / (isCoarsePointer ? 4 : 5)
+
+		function createLemon(x: number, y: number, deltaX: number, deltaY: number) {
+			const H = root.offsetHeight
+			if (y > H - 100) return
+
+			const img = document.createElement('img')
+			img.src = LEMONS[indexImg]
+			img.style.cssText = 'top:0;left:0;width:15vw;height:15vw;position:absolute;object-fit:contain;z-index:20;pointer-events:none;'
+			root.appendChild(img)
+
+			const tl = gsap.timeline({
+				onComplete: () => {
+					root.removeChild(img)
+					tl.kill()
+				}
+			})
+
+			tl.fromTo(img, {
+				xPercent: -50 + (Math.random() - 0.5) * 80,
+				yPercent: -50 + (Math.random() - 0.5) * 10,
+				rotation: (Math.random() - 0.5) * 20
+			}, {
+				ease: 'elastic.out(2, 0.6)',
+				duration: 0.5
+			})
+
+			tl.fromTo(img, { x }, {
+				x: '+=' + deltaX * 2,
+				rotation: 0,
+				ease: 'power1.in',
+				duration: 0.5
+			}, '<')
+
+			tl.fromTo(img, { y }, {
+				y: '+=' + (H - y),
+				scale: 0.9,
+				yPercent: -95,
+				ease: 'back.in(1.1)',
+				duration: 0.5
+			}, '<')
+
+			tl.to(img, {
+				x: '+=' + deltaX * 1.6,
+				rotation: (Math.random() - 0.5) * 40,
+				ease: 'power1.in',
+				duration: 0.4
+			})
+
+			tl.to(img, {
+				yPercent: 150,
+				ease: 'back.in(' + (1.5 + (1 - y / H)) + ')',
+				duration: 0.4
+			}, '<')
+
+			indexImg = (indexImg + 1) % LEMONS.length
+		}
+
+		function applyMove(clientX: number, clientY: number) {
+			const rect = root.getBoundingClientRect()
+
+			if (firstMove) {
+				firstMove = false
+				oldIncrX = clientX
+				oldIncrY = clientY
+				return
+			}
+
+			incr += Math.abs(clientX - oldIncrX) + Math.abs(clientY - oldIncrY)
+
+			if (incr > resetDist) {
+				incr = 0
+				createLemon(
+					clientX - rect.left,
+					clientY - rect.top,
+					clientX - oldIncrX,
+					clientY - oldIncrY
+				)
+			}
+
+			oldIncrX = clientX
+			oldIncrY = clientY
+		}
+
+		const onMouseMove = (e: MouseEvent) => applyMove(e.clientX, e.clientY)
+		const onTouchMove = (e: TouchEvent) => {
+			if (e.touches?.[0]) applyMove(e.touches[0].clientX, e.touches[0].clientY)
+		}
+
+		root.addEventListener('mousemove', onMouseMove)
+		root.addEventListener('touchstart', onTouchMove, { passive: true })
+		root.addEventListener('touchmove', onTouchMove, { passive: true })
+
+		return () => {
+			root.removeEventListener('mousemove', onMouseMove)
+			root.removeEventListener('touchstart', onTouchMove)
+			root.removeEventListener('touchmove', onTouchMove)
+		}
+	}, [containerRef])
+}
+
+const LemonTrail = function LemonTrail({
+	as: Tag = 'div',
+	className,
+	children,
+	...props
+}: LemonTrailProps) {
+	const containerRef = useRef<HTMLElement>(null)
+
+	useLemonTrail(containerRef)
+
+	return (
+		<Tag
+			ref={containerRef}
+			className={className}
+			{...props}
+		>
+			{children}
+		</Tag>
+	)
+}
+export default LemonTrail
+
